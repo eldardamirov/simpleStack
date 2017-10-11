c9019cc14219dd5c5274613570637d460f963eb0
eldardamirov <eldardamirov75@mail.ru>
2017-10-11 21:41:57 +0300

    Initial Commit

---
+++ stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stack
+//
+//  Created by Эльдар Дамиров on 11.10.2017.
+//  Copyright © 2017 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
