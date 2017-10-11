cb06bfa7f39d2507c8967129bda9bb084f8afb6d
eldardamirov <eldardamirov75@mail.ru>
2017-10-12 01:32:34 +0300

    Initial commit

---
+++ stack/fileIO.h
@@ -0,0 +1,272 @@
+//
+//  fileIO.h
+//  stack
+//
+//  Created by Эльдар Дамиров on 12.10.2017.
+//  Copyright © 2017 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef fileIO_h
+#define fileIO_h
+
+
+#include <sys/uio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <time.h>
+
+
+#include <iostream>
+
+
+
+
+struct fileMapping
+    {
+    int fileDescriptor;
+    size_t fileSize;
+    unsigned char* dataPointer;
+    };
+
+
+class readFromFile
+    {
+    public:
+
+        readFromFile ( char* inputFileName )
+            {
+            inputFileName = "input.txt";
+
+            init();
+            }
+
+        ~readFromFile()
+            {
+            munmap ( dataPointer, fileSize );
+            close ( mapping->fileDescriptor );
+            free ( mapping );
+            }
+
+
+
+        char getNextChar()
+            {
+            char tempBuffer =  *( ( mapping->dataPointer ) + currentChar );
+            currentChar++;
+
+            return tempBuffer;
+            }
+
+
+        size_t getFileSize()
+            {
+            return fileSize;
+            }
+
+
+        char* meGetDataPointer()
+            {
+            return ( char* ) ( mapping->dataPointer );
+            }
+
+
+        bool isEnd()
+            {
+            return ( currentChar + 1 ) >= fileSize;
+            }
+
+    private:
+
+        char* inputFileName = {};
+        struct stat st;
+        size_t fileSize = 0;
+        int fileDescriptor = 0;
+        unsigned char* dataPointer = {};
+        fileMapping* mapping;
+        int currentChar = 0;
+
+        void init()
+            {
+           
+            fileDescriptor = open ( "input.txt", O_RDWR , 0 );
+            if ( fileDescriptor < 0 )
+                {
+                printf ( "File descriptor error.\n" );
+                abort();
+                }
+
+
+            if ( fstat ( fileDescriptor, &st ) < 0 )
+                {
+                printf ( "Fstat failed.\n" );
+                close ( fileDescriptor );
+                abort();
+                }
+            fileSize = ( size_t ) st.st_size;
+
+            dataPointer = getDataPointer ( fileSize, fileDescriptor );
+            mapping = createFileMapping ( fileDescriptor, dataPointer, fileSize );
+
+
+            }
+
+        unsigned char* getDataPointer ( size_t fileSize, int fileDescriptor )
+            {
+            unsigned char* tempDataPointer = ( unsigned char* ) mmap ( nullptr, fileSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fileDescriptor, 0 );
+
+            if ( tempDataPointer == MAP_FAILED )
+                {
+                printf ( "mmap failed\n" );
+                close ( fileDescriptor );
+                abort();
+                }
+
+            return tempDataPointer;
+            }
+
+        fileMapping* createFileMapping ( int fileDescriptor, unsigned char* dataPointer, size_t fileSize )
+            {
+            fileMapping* tempMapping = ( fileMapping* ) malloc ( sizeof ( fileMapping ) );
+
+            if ( tempMapping == nullptr )
+                {
+                printf ( "mmap failed\n" );
+                munmap ( dataPointer, fileSize );
+                close ( fileDescriptor );
+                abort();
+                }
+
+            tempMapping->fileDescriptor = fileDescriptor;
+            tempMapping->fileSize = fileSize;
+            tempMapping->dataPointer = dataPointer;
+
+            return tempMapping;
+            }
+
+
+    };
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+class writeToFile
+    {
+    public:
+
+        writeToFile ( char* fileName, size_t fileSize )
+            {
+            outputFileName = fileName;
+            fileSizeMe = fileSize + 1;
+
+            init();
+            }
+
+
+        void writeNextChar ( char inputChar )
+            {
+            map [ currentPosition ] = inputChar;
+            currentPosition++;
+            }
+
+        ~writeToFile()
+            {
+            msync ( map, fileSizeMe, MS_SYNC );
+            munmap ( map, fileSizeMe );
+            close ( fileDescriptor );
+
+            }
+
+
+    private:
+
+        char* outputFileName = {};
+        struct stat st;
+        size_t fileSizeMe = 0;
+        int fileDescriptor = 0;
+        int fileDescription = 0;
+        char* map;
+        int currentPosition = 0;
+
+
+
+        void init()
+            {
+            fileDescriptor = open ( "dump.txt", O_RDWR | O_CREAT , ( mode_t ) 0600 );
+            if ( fileDescriptor < 0 )
+                {
+                printf ( "File descriptor error.\n" );
+                abort();
+                }
+
+            fileDescription = makeFileDescription ( fileDescriptor, fileSizeMe );
+            updateFileSize ( fileDescriptor, fileDescription );
+            map = mmapFile ( fileDescriptor, fileSizeMe );
+
+            }
+
+
+        int makeFileDescription ( int fileDescriptor, size_t fileSize )
+            {
+            
+            int tempFileDescription = lseek ( fileDescriptor, fileSizeMe, SEEK_SET );
+            if ( tempFileDescription < 0 )
+                {
+                close ( fileDescriptor );
+                printf ( "lseek error." );
+                abort();
+                }
+
+            return tempFileDescription;
+            }
+
+        void updateFileSize ( int fileDescriptor, int fileDescription )
+            {
+            if ( write ( fileDescriptor, "", 1 ) < 0 )
+                {
+                close ( fileDescriptor );
+                printf ( "Writing null string to last file's byte failed." );
+                abort();
+                }
+            }
+
+
+
+        char* mmapFile ( int fileDescriptor, size_t fileSize )
+            {
+            char* tempMap = ( char* ) ( mmap ( 0, fileSize, PROT_WRITE | PROT_READ, MAP_SHARED, fileDescriptor, 0 ) );
+            if ( tempMap == MAP_FAILED )
+                {
+                close ( fileDescriptor );
+                printf ( "File mapping failed." );
+                abort();
+                }
+
+            return tempMap;
+            }
+
+
+
+
+    };
+
+
+#endif /* fileIO_h */
--- stack/main.cpp
+++ stack/main.cpp
@@ -6,10 +6,320 @@
 //  Copyright © 2017 Эльдар Дамиров. All rights reserved.
 //
 
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <time.h>
+
+
+#include "fileIO.h"
+
+
+
+
+
+class stack
+    {
+    public:
+        
+        typedef int typeOfData;
+        
+        
+        stack ( size_t newStackCapacity )
+            {
+            stackCapacity = newStackCapacity;
+            init();
+            }
+        
+        ~stack()
+            {
+            
+            }
+        
+
+        
+        typeOfData* getBeginningOfStack()
+            {
+            if ( beginningOfStack == nullptr )
+                {
+                printf ( "Stack isn't initialized, or some error during init has occured. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In getBeginningOfStack: " + "Stack isn't initialized, or some error during init has occured. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+                return nullptr;
+                }
+            return beginningOfStack;
+            }
+        
+        typeOfData* getCurrentFreeElement()
+            {
+            if ( ( currentFreeElement == nullptr ) || ( currentFreeElement < beginningOfStack ) )
+                {
+                printf ( "Current free element is out of range or even hasn't been initialized, maybe something is wrong with stack pointer. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In getCurrentFreeElement: " + "Current free element is out of range or even hasn't been initialized, maybe something is wrong with stack pointer. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+                return nullptr;
+                }
+                
+            return currentFreeElement;
+            }
+        
+        size_t getStackCapacity()
+            {
+            if ( stackCapacity == 0 )
+                {
+                printf ( "Something is wrong with stack storage or stack capacity value hasn't been updated. ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In getStackCapacity: " + "Something is wrong with stack storage or stack capacity value hasn't been updated. ¯|_(ツ)_/¯ \n";
+                return -1;
+                }
+                
+            return stackCapacity;
+            }
+        
+        int setBeginningOfStack ( typeOfData* newBeginningOfStack )
+            {
+            if ( newBeginningOfStack == nullptr )
+                {
+                printf ( "New beginning of stack is incorrect. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In setBeginningOfStack: " + "New beginning of stack is incorrect. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+
+                return -1;
+                }
+            
+            beginningOfStack = newBeginningOfStack;
+            
+            return 0;
+            }
+        
+        int setCurrentFreeElement ( typeOfData* newCurrentFreeElement )
+            {
+            if ( newCurrentFreeElement == nullptr )
+                {
+                printf ( "New current free element is incorrect. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In setCurrentFreeElement: " + "New current free element is incorrect. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+
+                return -1;
+                }
+                
+            currentFreeElement = newCurrentFreeElement;
+            
+            return 0;
+            }
+        
+        int setStackCapacity ( size_t newStackCapacity )
+            {
+            if ( newStackCapacity == 0 )
+                {
+                printf ( "Something is wrong with stack storage or stack capacity value hasn't been updated. ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In setStackCapacity: " + "Something is wrong with stack storage or stack capacity value hasn't been updated. ¯|_(ツ)_/¯ \n";
+                return -1;
+                }
+                
+            stackCapacity = newStackCapacity;
+                
+            return 0;
+            }
+        
+        int push ( typeOfData elementToPush )
+            {
+            if ( currentFreeElement > ( beginningOfStack + ( stackCapacity - 1 ) ) )
+                {
+                printf ( "Bad access, trying to access element out of range. ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In PUSH: " + "Bad access, trying to access element out of range. ¯|_(ツ)_/¯ \n";
+                return -1;
+                }
+            
+            *currentFreeElement = elementToPush;
+            currentFreeElement++;
+            
+            return 0;
+            }
+        
+        int clear()
+            {
+            if ( ( currentFreeElement == nullptr ) || ( currentFreeElement == nullptr ) )
+                {
+                printf ( "Stack isn't initialized, or some error during init has occured. ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In CLEAR: " + "Stack isn't initialized, or some error during init has occured. ¯|_(ツ)_/¯ \n";
+                return -1;
+                }
+                
+            currentFreeElement = beginningOfStack;
+            
+            return 0;
+            }
+        
+        typeOfData* top()
+            {
+            if ( ( ( currentFreeElement - 1 ) == nullptr ) || ( beginningOfStack == nullptr ) )
+                {
+                printf ( "Current free element is out of range or even hasn't been initialized, maybe something is wrong with stack pointer. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In TOP: " + "Current free element is out of range or even hasn't been initialized, maybe something is wrong with stack pointer. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+
+                return nullptr;
+                }
+            
+            if ( ( currentFreeElement - 1 ) < beginningOfStack )
+                {
+                return beginningOfStack;
+                }
+            else
+                {
+                return ( currentFreeElement - 1 );
+                }
+                
+            }
+        
+        int pop()
+            {
+            if ( ( currentFreeElement - 1 ) <  beginningOfStack )
+                {
+                printf ( "Current top element is out of range or even hasn't been initialized, maybe something is wrong with stack pointer. ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In POP: " + "Current top element is out of range or even hasn't been initialized, maybe something is wrong with stack pointer. ¯|_(ツ)_/¯ \n";
+
+                return -1;
+                }
+                
+            currentFreeElement--;
+            
+            return 0;
+            }
+        
+        bool empty()
+            {
+            if ( ( currentFreeElement == nullptr ) || ( currentFreeElement == nullptr ) )
+                {
+                printf ( "Stack isn't initialized, or some error during init has occured. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In EMPTY: " + "Stack isn't initialized, or some error during init has occured. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+
+                return false;
+                }
+            
+            if ( currentFreeElement == beginningOfStack )
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+        
+        int size()
+            {
+            if ( ( beginningOfStack == nullptr ) || ( currentFreeElement < beginningOfStack ) || ( currentFreeElement == nullptr ) )
+                {
+                printf ( "Stack isn't initialized, or some error during init has occured. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In SIZE: " + "Stack isn't initialized, or some error during init has occured. (Note that returning value in this case isn't prorep one) ¯|_(ツ)_/¯ \n";
+
+                return -1;
+                }
+                
+            int currentSize = ( currentFreeElement - beginningOfStack );
+            
+            return currentSize;
+            }
+        
+        int swap ( stack stackToSwapWith )
+            {
+            typeOfData* tempBeginningOfStack = stackToSwapWith.getBeginningOfStack();
+            typeOfData* tempCurrentFreeElement = stackToSwapWith.getCurrentFreeElement();
+            size_t tempStackCapacity = stackToSwapWith.getStackCapacity();
+            
+            if ( ( tempBeginningOfStack == nullptr ) || ( tempCurrentFreeElement == nullptr ) || ( tempStackCapacity == 0 ) )
+                {
+                printf ( "One of the swapping stacks isn't initialised or working wrong. ¯|_(ツ)_/¯ \n" );
+                listOfErrors = listOfErrors + "In SWAP: " + "One of the swapping stacks isn't initialised or working wrong. ¯|_(ツ)_/¯ \n";
+                
+                return -1;
+                }
+                
+            stackToSwapWith.setBeginningOfStack ( beginningOfStack );
+            stackToSwapWith.setCurrentFreeElement ( currentFreeElement );
+            stackToSwapWith.setStackCapacity ( stackCapacity );
+            
+            beginningOfStack = tempBeginningOfStack;
+            currentFreeElement = tempCurrentFreeElement;
+            stackCapacity = tempStackCapacity;
+            
+            return 0;
+            }
+        
+        
+        int dump()
+            {
+            writeToFile dumpOutput ( "dump.txt", 10000 );
+
+            
+            std::string firstLine = "Errors: ";
+            if ( listOfErrors.size() == 0 )
+                {
+                firstLine = firstLine + "none of them. \n";
+                }
+            else
+                {
+                firstLine = firstLine + "\n";
+                }
+            
+            for ( int i = 0; i < firstLine.size(); i++ )
+                {
+                dumpOutput.writeNextChar ( firstLine [ i ] );
+                }
+            
+            for ( int i = 0; i < listOfErrors.size(); i++ )
+                {
+                dumpOutput.writeNextChar ( 'f' );
+                }
+
+            return 0;
+            }
+            
+        
+    
+    private:
+        typeOfData* beginningOfStack = nullptr;
+
+        size_t stackCapacity = 0;
+        typeOfData* currentFreeElement = nullptr;
+        std::string listOfErrors = "";
+        
+        int init()
+            {
+            beginningOfStack = ( typeOfData* ) calloc ( stackCapacity, sizeof ( typeOfData ) );
+            
+            if ( beginningOfStack == nullptr )
+                {
+                printf ( "Beginning pointer isn't correct. ¯|_(ツ)_/¯ \n" );
+                return -1;
+                }
+            
+            currentFreeElement = beginningOfStack;
+            
+            return 0;
+            }
+        
+        
+    };
+
+
+
+int main(int argc, const char * argv[])
+    {
+    
+
+    
+    stack myStack ( 2 );
+    myStack.push ( 53 );
+    myStack.push ( 46 );
+    myStack.pop();
+    myStack.push ( 5348 );
+    
+    stack myStack2 ( 1 );
+    myStack2.push ( 28379 );
+    myStack2.push ( 4727 );
+    
+    myStack.swap ( myStack2 );
+    
+    myStack.dump();
+    
+    printf ( "%d", myStack.size() );
+    
     return 0;
-}
+    }
